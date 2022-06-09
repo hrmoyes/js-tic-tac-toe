a4e059b0ea7421998984efcb2797beee74b7642c
hrmoyes <hrmoyes@gmail.com>
2022-06-09 13:06:32 -0500

    Added win styling functionality
    Added change color on win, and turn counter + turn preventer for wrong player.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 let currentChoice = "X";
 let trustArray = [1, 1, 1, 1, 1, 1, 1, 1, 1];
 let preventPress = "You already made a turn there!";
-
+let turnCounter = 1;
 
 function playerChooseX() {
     currentChoice = "X"
@@ -11,11 +11,24 @@ function playerChooseO() {
 }
 
 function clickBlock(blockNum) {
-    if (trustArray[blockNum - 1] == true) {
-        document.getElementById(blockNum).value = currentChoice;
-        trustArray[blockNum - 1] = 0;
+    if ((turnCounter == 1 && currentChoice == "X") || (turnCounter == 0 && currentChoice == "O")) {
+        if (trustArray[blockNum - 1] == true) {
+            document.getElementById(blockNum).value = currentChoice;
+            trustArray[blockNum - 1] = 0;
+            if (currentChoice == "X") {
+                let whoseTurn = document.getElementById("turn-indic") 
+                whoseTurn.textContent = `It is O's turn to go.`;
+                turnCounter = 0;
+            } else if (currentChoice == "O") {
+                let whoseTurn = document.getElementById("turn-indic") 
+                whoseTurn.textContent = `It is X's turn to go.`;
+                turnCounter = 1;
+            }
+        } else {
+            alert(preventPress);
+        }
     } else {
-        alert(preventPress);
+        alert(`It's not your turn, please let other player go first!`)
     }
 }
 
@@ -38,9 +51,22 @@ function winChecker() {
     let s8 = document.getElementById("8").value;
     let s9 = document.getElementById("9").value;
 
+    let s1Style = document.getElementById("1");
+    let s2Style = document.getElementById("2");
+    let s3Style = document.getElementById("3");
+    let s4Style = document.getElementById("4");
+    let s5Style = document.getElementById("5");
+    let s6Style = document.getElementById("6");
+    let s7Style = document.getElementById("7");
+    let s8Style = document.getElementById("8");
+    let s9Style = document.getElementById("9");
+
 
     if ((s1 == s2 && s2 == s3) && s1 != "") {
         alert(`Player ${s1} won!`);
+        s1Style.style.color = "green";
+        s2Style.style.color = "green";
+        s3Style.style.color = "green";
         gameOver();
     } else if ((s1 == s4 && s4 == s7) && s1 != "") {
         alert(`Player ${s1} won!`);
